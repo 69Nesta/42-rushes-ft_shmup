8c46a4e42b674fc101c089fe5fd63e79455b9fb0
RoiShigai <roishigai2@gmail.com>
2025-11-29 22:35:02 +0100

    entity update

--- src_curses/entity.cpp
+++ src_curses/entity.cpp
@@ -11,6 +11,16 @@ Entity	init_player(unsigned int cols, unsigned int lines)
 	return (Player(cols, lines));
 }
 
+//  Entity check position && collision
+
+
+bool    Entity::check_collision(Entity a, Entity b)
+{
+    if (a.get_cols == b.get_cols && a.get_lines == b.get_lines)
+        return (TRUE);
+    return (FALSE);
+}
+
 unsigned int	Entity::get_lines()
 {
 	return (Entity::lines);
@@ -26,6 +36,10 @@ char	Entity::get_ship()
 	return (Entity::ship);
 }
 
+
+//  Entity Movement update
+
+
 void	Entity::increment_entity_lines()
 {
 	Entity::lines -= 1;
@@ -36,15 +50,53 @@ void	Entity::decrement_entity_lines()
 	Entity::lines += 1;
 }
 
+void    Entity::increment_entity_cols()
+{
+    Entity::cols += 1;
+}
+
+void    Entity::decrement_entity_cols()
+{
+    Entity::cols -= 1;
+}
+
+// Projectile Entity functions
+
+Projectile (unsigned int cols, unsigned int lines, int direction) 
+{
+			this->cols = cols;
+			this->lines = lines;
+			this->direction = direction;
+			this->ship = '-';
+            return (Projectile);
+}
+
+
 Projectile   Player::shoot(Player player)
 {
-	Projectile projectile(player.cols, player.lines, 1);
+	Projectile projectile(player.cols + 1, player.lines, 1);
 
 	return (projectile);
 }
 
 Projectile	Ennemy::shoot(Ennemy ennemy)
 {
-	Projectile projectile(ennemy.cols, ennemy.lines, -1);
+	Projectile projectile(ennemy.cols - 1, ennemy.lines, -1);
 	return (projectile);
+}
+
+// Player   function
+
+void    Player::player_get_hit(bool hit, gamestate::health health)
+{
+    if (hit == TRUE)
+        health--;
+}
+
+// Ennemy function
+
+void    Ennemy::ennemy_get_hit(bool hit, gamestate::score score)
+{
+    if (hit == TRUE)
+        score += 100;
 }
