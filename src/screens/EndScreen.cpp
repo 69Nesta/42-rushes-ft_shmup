628ed4bd86855a36b1a94299a2b0537a8bfe5042
69Nesta <petitromeo74@gmail.com>
2025-11-30 23:13:16 +0100

    Feat: add game over message

--- src/screens/EndScreen.cpp
+++ src/screens/EndScreen.cpp
@@ -1,5 +1,6 @@
 #include "screens/EndScreen.hpp"
 #include <string>
+#include <sstream>
 
 EndScreen::EndScreen(IScreenManager& screen_manager, GameStateManager &game_state, bool& is_running):
 	Screen(screen_manager, ScreenType::END), game_state(game_state), is_running(is_running)
@@ -25,21 +26,38 @@ void	EndScreen::update(float delta_time)
 void	EndScreen::render(void)
 {}
 
+std::string game_over_msg = "   _________    __  _________   ____ _    ____________ \n"
+"  / ____/   |  /  |/  / ____/  / __ \\ |  / / ____/ __ \\\n"
+" / / __/ /| | / /|_/ / __/    / / / / | / / __/ / /_/ /\n"
+"/ /_/ / ___ |/ /  / / /___   / /_/ /| |/ / /___/ _, _/ \n"
+"\\____/_/  |_/_/  /_/_____/   \\____/ |___/_____/_/ |_|  \n";
+
 void	EndScreen::resize()
 {
-	erase();
-	std::string end_msg = "GAME OVER !";
-	std::ostringstream	score;
-	score << "SCORE: " << this->game_state.get_score();
-	std::string score_s = std::to_string(game_state.get_score());
+    erase();
+    // utiliser le message multi-lignes game_over_msg
+    std::ostringstream	score;
+    score << "SCORE: " << this->game_state.get_score();
 
-	start_color();
-	init_pair(1, COLOR_YELLOW, COLOR_BLACK);
-	mvwprintw(this->window, LINES / 5 * 2, (COLS / 2) - end_msg.length() / 2, "%s", end_msg.c_str());
-	mvwprintw(this->window, LINES / 5 * 2 + 2, (COLS / 2) - score.str().length() / 2, "%s", score.str().c_str());
-	box(this->window, ACS_VLINE, ACS_HLINE);
-	wbkgd(this->window, COLOR_PAIR(1));
-	wrefresh(this->window);
+    start_color();
+    init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+
+    // découpe game_over_msg en lignes et affiche chaque ligne centrée
+    std::istringstream iss(game_over_msg);
+    std::string line;
+    int y = LINES / 5 * 2;
+    while (std::getline(iss, line))
+    {
+        int x = (COLS - static_cast<int>(line.length())) / 2;
+        if (x < 0) x = 0;
+        mvwprintw(this->window, y++, x, "%s", line.c_str());
+    }
+
+    // affiche le score juste en dessous du bloc du message
+    mvwprintw(this->window, y + 1, (COLS / 2) - score.str().length() / 2, "%s", score.str().c_str());
+    box(this->window, ACS_VLINE, ACS_HLINE);
+    wbkgd(this->window, COLOR_PAIR(1));
+    wrefresh(this->window);
 }
 
 void	EndScreen::handle_input(InputHandler& input)
